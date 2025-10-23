7fd8ee346e1e6d1ca8ad9ab38642cb56f7b1d053
Andrew Merritt <a0merr05@louisville.edu>
2025-10-23 08:46:23 -0400

    BST.h

--- BST.h
+++ BST.h
@@ -3,34 +3,36 @@
 
 class BST {
 private:
-  struct Node {
-    int key;
-    Node* left;
-    Node* right;
-};
+    struct Node {
+        int key;
+        Node* left;
+        Node* right;
+    };
 
-Node* root;
-//Helper functions//
-Node8 InsertHelper(Node* node, int value);
-bool SearchHelper(Node8 node, int value) const;
-void PrintInOrderHelper(Node8 node) const;
-void Destory(Node* node);
-// GetHeight helpers//
-int GetHeightHelper(Node8 node) const;
-//splitBalance helpers//
-void InOrderStore(Node* node, int arr[], int& index);
-Node* BuildBalancedTree(int arr[], int start, int end);
+    Node* root;
+
+    // Helper functions
+    Node* InsertHelper(Node* node, int value);
+    bool SearchHelper(Node* node, int value) const;
+    void PrintInOrderHelper(Node* node) const;
+    void Destroy(Node* node);
+    int GetHeightHelper(Node* node) const;
+    void InOrderStore(Node* node, int arr[], int& index);
+    Node* BuildBalancedTree(int arr[], int start, int end);
 
 public:
-BST();
-~BST();
+    BST();
+    ~BST();
 
-void Insert(int value);
-bool Search(int value) const;
-void PrintInorder() const;
-// Required new functions//
-int GetHeight() const;
-void SplitBalance();
+    // Match assignment/test names
+    void MakeEmpty();          // clears the tree
+    void PutItem(int value);   // insert item
+    bool Search(int value) const;
+    void PrintInOrder() const;
+
+    // Assignment-required functions
+    int GetHeight() const;
+    void SplitBalance();
 };
 
 #endif
